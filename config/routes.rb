e71d6c36595e0824e0b25b16022487399ae83bb4
bhupendra9211 <shahbhupendra9211@gmail.com>
2024-03-16 20:25:48 +0545

    Adding pages route manually

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
 Rails.application.routes.draw do
-  get "home/index"
-  # get '/home' => 'home#index'
+  # get "home/index"
+  # get 'home' => 'home#index'
   root 'home#index'
+
+  # get 'pages/about_us'
+  # get 'pages/contact_us'
+  # get 'pages/privacy_policy'
+  # get 'pages/terms_and_condition'
+
+  #-- Custome/Public pages route.......
+  get 'about'=> 'pages#about_us'
+  get 'contact' => 'pages#contact_us'
+  get 'privacy-policy' => 'pages#privacy_policy'
+  get 'terms-and-conditions' => 'pages#terms_and_conditions'
+
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
