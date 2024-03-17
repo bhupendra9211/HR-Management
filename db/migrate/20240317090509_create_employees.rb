66f25f01c856e568a96ee0300379b07e16f56847
bhupendra9211 <shahbhupendra9211@gmail.com>
2024-03-17 14:53:06 +0545

    Creating our first model name employee

---
+++ db/migrate/20240317090509_create_employees.rb
@@ -0,0 +1,16 @@
+class CreateEmployees < ActiveRecord::Migration[7.1]
+  def change
+    create_table :employees do |t|
+      t.string :first_name
+      t.string :middle_name
+      t.string :last_name
+      t.string :personal_email
+      t.string :city
+      t.string :country
+      t.string :address_line_1
+      t.string :address_line_2
+
+      t.timestamps
+    end
+  end
+end
