ae78a2bcb537d152651c316ced9510772b455ed0
bhupendra9211 <shahbhupendra9211@gmail.com>
2024-03-26 23:06:05 +0545

    dd

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,5 +2,12 @@ class Employee < ApplicationRecord
     validates :first_name, :last_name, presence: true
     validates :personal_email, presence: true, uniqueness: true
     validates :city, :country, :address_line_1, presence: true
+
+    def name 
+        "#{first_name} #{middle_name} #{last_name}".strip
+    end
+    def full_address
+        "#{address_line_1},#{city}".strip
+    end
 end
 
