4a60f245fbf6e43b17ca7ce3bd22586bf31a1acd
bhupendra9211 <shahbhupendra9211@gmail.com>
2024-03-28 00:20:14 +0545

    adding edit new

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -3,6 +3,28 @@ class EmployeesController < ApplicationController
     @employees = Employee.all
   end
   def new
-    @employees = Employee.new
+    @employee = Employee.new
   end
+  def create
+    # @employee = Employee.new(first_name: params[:employee] [:first_name],
+    # middle_name: params[:employee] [:middle_name],last_name: params[:employee] [:last_name],
+    # personal_email: params[:employee] [:personal_email],country: params[:employee] [:country],
+    # city: params[:employee] [:city],address_line_1: params[:employee] [:address_line_1],address_line_2: params[:employee] [:address_line_2])
+    @employee = Employee.new(employee_params)
+    if @employee.save
+      redirect_to employees_path, notice: 'Employee has been created sucessfully'
+    else
+      render :new
+    end
+  end
+  def edit
+    @employee = Employee.find(params[:id])
+  end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:first_name, :middle_name, :last_name, :personal_email, :country, :city, :address_line_1, :address_line_2)
+  end
+
 end
