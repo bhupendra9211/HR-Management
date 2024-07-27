0d8e6dfaba1bcc7d8f0f9d2e88070918c361ffad
bhupendra9211 <shahbhupendra9211@gmail.com>
2024-07-27 15:41:30 +0545

    adding employee details

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,5 @@
 class Employee < ApplicationRecord
+  validates :first_name, :last_name, presence: true
+  validates :personal_email ,presence: true , uniqueness: true
+  validates :district, :zone , :province , :temporary_address , :permanent_address , :country , presence:true 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,39 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Employee.create(
+  first_name: 'Sibani',
+  middle_name: "Kumari",
+  last_name: "Sah",
+  personal_email: "dfdfdni@gmail.com",
+  district: "dhanush",
+  zone:"janakpur",
+  province:"2",
+  temporary_address: "lokanthali",
+  permanent_address: "dhanusha",
+  country: "nepal"
+ )
+Employee.create(
+  first_name: 'Ashok',
+  middle_name: "Kumar",
+  last_name: "Sah",
+  personal_email: "ashok@gmail.com",
+  district: "dhanush",
+  zone:"janakpur",
+  province:"2",
+  temporary_address: "lokanthali",
+  permanent_address: "dhanusha",
+  country: "nepal"
+ )
+Employee.create(
+  first_name: 'Ram',
+  middle_name: "Kumar",
+  last_name: "Sah",
+  personal_email: "ram@gmail.com",
+  district: "dhanush",
+  zone:"janakpur",
+  province:"2",
+  temporary_address: "lokanthali",
+  permanent_address: "dhanusha",
+  country: "nepal"
+ )
